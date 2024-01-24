51df3a50e8c6c0eeebc2344a0ba13db67f897f4a
Luchianno Mascarino <125408126+Luchianno-Mascarino@users.noreply.github.com>
2024-01-24 16:44:21 -0300

    ADD route to get random quote ADD route to get all the quotes and a specific quote ADD route for adding new quotes ADD PORT listener

--- server.js
+++ server.js
@@ -8,3 +8,36 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.get("/api/quotes/random", (req, res, next) => {
+  const randomQuote = getRandomElement(quotes);
+  res.send({
+    quote: randomQuote,
+  });
+});
+
+app.get('/api/quotes', (req, res) => {
+    const filterQuotes = quotes.filter(author => {
+      return author.person === req.query.person;
+    });
+    if (req.query.person) {
+      res.send({ quotes: filterQuotes });
+    } else {
+      res.send({ quotes: quotes });
+    }
+  });
+
+app.post('/api/quotes', (req, res) => {
+  const newQuote = req.query.quote;
+  const newPerson = req.query.person;
+  if (newQuote != '' && newPerson != '') {
+    quotes.push({ quote: newQuote, person: newPerson });
+    res.send({ quote: { quote: newQuote, person: newPerson } });
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+
+app.listen(PORT, () =>{
+    console.log(`App listening to port ${PORT}`)
+})
